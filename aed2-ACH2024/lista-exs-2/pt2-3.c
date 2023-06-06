995e71f25e91bd6c7c9d4a370a013f8dbd622262
Victor Augusto <victorcosta939@gmail.com>
2023-06-06 14:36:32 -0300

    lista de exercicios 2 - pt2 ex 3

---
+++ aed2-ACH2024/lista-exs-2/pt2-3.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "./basics.c"
+
+int main() {
+    int curso = 0;
+
+    // cria ponteiro para arquivo
+    FILE* arq1;
+    // abre arquivo no modo de leitura
+    arq1 = fopen("origem.bin", "rb");
+    // verifica se o arquivo abre com sucesso
+    // tratativa de erro
+    if (arq1 == NULL) {
+        printf("Arquivo não abriu.\n");
+        exit(0);
+    }
+
+    //Visualizar o arquivo de origem
+    printf("ARQUIVO DE ORIGEM\n");
+    printFile(arq1);
+
+    FILE* arq2;
+    arq2 = fopen("destino.bin", "wb");
+    if (arq2 == NULL) {
+        printf("Arquivo não abriu.\n");
+        exit(0);
+    }
+
+    // Criar a tabela de índices primários
+    TABELA tabela[V];
+    REGISTRO r;
+    int prox = 0;
+    fseek(arq1, 0, SEEK_SET);
+    while (fread(&r, sizeof(REGISTRO), 1, arq1) == 1) {
+        inserirIndice(tabela, r.curso, prox);
+        prox++;
+    }
+    
+    // Escrever no arquivo de destino os registros
+    for (int i = 0; i < prox; i++) {
+        if (tabela[i].chave != curso) {
+            fseek(arq1, sizeof(REGISTRO) * tabela[i].end, SEEK_SET);
+            fread(&r, sizeof(REGISTRO), 1, arq1);
+            fwrite(&r, sizeof(REGISTRO), 1, arq2);
+        }
+    }
+
+    fclose(arq1);
+    fclose(arq2);
+
+    // Leitura para ver se o arquivo destino está certo
+    FILE* f = fopen ("destino.bin", "rb");
+    if (f != NULL) {
+        printf("\nARQUIVO DE DESTINO\n");
+        printFile(f);
+        fclose (f);
+    }
+
+    return 1;
+}
